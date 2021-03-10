070edbd86ca24d0e51e57b8c0945b6fd0a3c8ea7
Adam Fulford <fulford.adam@gmail.com>
2021-03-09 19:55:45 -0500

    cleaning up

--- QSPI_Settings.cpp
+++ QSPI_Settings.cpp
@@ -3,13 +3,11 @@
 #include <math.h>
 
 #define BUFF_SIZE (sizeof(Settings)/sizeof(float))
-//using namespace daisy;
 
-static uint32_t DSY_QSPI_BSS membuff[(4 * BUFF_SIZE) + 1];   //block of memory in flash
+//block of memory in flash
+static uint32_t DSY_QSPI_BSS membuff[(4 * BUFF_SIZE) + 1];   
 
-
-//constexpr uint32_t base = 0x90000000;
-uint32_t base = (uint32_t) &membuff[0]; //this gives same address as above
+uint32_t base = (uint32_t) &membuff[0];
 
 //save data to flash memory
 int SaveSettings(const Settings &currentSetting)
@@ -27,7 +25,6 @@ int SaveSettings(const Settings &currentSetting)
     
     //split into 8bit chunks:
     //skip first byte - out by one byte for some reason
-
     uint8_t writebuff[(4 * BUFF_SIZE) + 1];
     writebuff[0] = 0; //ignore first value?
     for(uint8_t i = 0; i < BUFF_SIZE; i++) 
@@ -39,22 +36,12 @@ int SaveSettings(const Settings &currentSetting)
         writebuff[(4*i) + 2] = bytes[1];
         writebuff[(4*i) + 3] = bytes[2];
         writebuff[(4*i) + 4] = bytes[3];
-        
-        /*
-        char32_t inbuff_int{char32_t(inbuff[i])};
-        writebuff[(4*i) + 1] = (inbuff_int >> 24);
-        writebuff[(4*i) + 2] = (inbuff_int >> 16)  & 0xFF;
-        writebuff[(4*i) + 3] = (inbuff_int >>  8)  & 0xFF;
-        writebuff[(4*i) + 4] = (inbuff_int)        & 0xFF;
-        */
     }
 
     hw.qspi_handle.mode = DSY_QSPI_MODE_INDIRECT_POLLING;
 	dsy_qspi_init(&hw.qspi_handle);
     dsy_qspi_erase(base, base + ((BUFF_SIZE + 1) * sizeof(membuff[0])));
     return dsy_qspi_write(base, (BUFF_SIZE + 1) * sizeof(membuff[0]), (uint8_t*)writebuff);
-    //dsy_qspi_erase(base, base + 2);
-    //return dsy_qspi_write(base, 2, (uint8_t*)writebuff);
 }
 
 //retreive data from flash memory
@@ -67,13 +54,11 @@ Settings LoadSettings()
     hw.qspi_handle.mode = DSY_QSPI_MODE_DSY_MEMORY_MAPPED;
     dsy_qspi_init(&hw.qspi_handle);
 
-    //memcpy(outbuff,membuff,sizeof(membuff[0]) * TEST_BUFF_SIZE);
     memcpy((void*)&outbuff,(void*)membuff,sizeof(outbuff));
     uint8_t bytes[sizeof(float)];
 
     for(uint8_t i = 0; i < BUFF_SIZE; i++)
     {
-        //readbuff[i] = float((outbuff[4*i] << 24) | (outbuff[(4*i)+1] << 16) | (outbuff[(4*i)+2] << 8) | outbuff[(4*i)+3]);
         for (uint8_t j = 0; j < sizeof(float); j++)
         {
             bytes[j] = outbuff[(4*i) + j];
